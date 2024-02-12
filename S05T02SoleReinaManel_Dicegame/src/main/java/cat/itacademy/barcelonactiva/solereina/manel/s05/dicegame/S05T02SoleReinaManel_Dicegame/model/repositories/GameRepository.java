450bea59cb21a341d787d93d3790393473b68ed6
Manel <msoler92@gmail.com>
2024-02-12 13:41:09 +0100

    Added Repository annotations

--- S05T02SoleReinaManel_Dicegame/src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/dicegame/S05T02SoleReinaManel_Dicegame/model/repositories/GameRepository.java
+++ S05T02SoleReinaManel_Dicegame/src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/dicegame/S05T02SoleReinaManel_Dicegame/model/repositories/GameRepository.java
@@ -2,9 +2,10 @@ package cat.itacademy.barcelonactiva.solereina.manel.s05.dicegame.S05T02SoleRein
 
 import cat.itacademy.barcelonactiva.solereina.manel.s05.dicegame.S05T02SoleReinaManel_Dicegame.model.domain.GameEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface GameRepository extends JpaRepository<GameEntity, Integer> {
     List<GameEntity> findByPlayerId(int playerId);
     void deleteByPlayerId(int playerId);
--- S05T02SoleReinaManel_Dicegame/src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/dicegame/S05T02SoleReinaManel_Dicegame/model/repositories/PlayerRepository.java
+++ S05T02SoleReinaManel_Dicegame/src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s05/dicegame/S05T02SoleReinaManel_Dicegame/model/repositories/PlayerRepository.java
@@ -2,6 +2,8 @@ package cat.itacademy.barcelonactiva.solereina.manel.s05.dicegame.S05T02SoleRein
 
 import cat.itacademy.barcelonactiva.solereina.manel.s05.dicegame.S05T02SoleReinaManel_Dicegame.model.domain.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Integer> {
 }
